bad62affc0b4bd932b5ac96c796149cd543c04e5
bigseoulNoteBook <bigseoul@gmail.com>
2023-02-27 14:37:56 +0900

    remove a trash

--- Chapter01/step01/bag_temp.py
+++
@@ -1,7 +0,0 @@
-class Bag:
-    def __init__(self, amount, invitation=None) -> None:
-        self.__amount = amount
-        if invitation == None:
-            self.__invitation = None
-        else:
-            self.__invitation = invitation
