1c5f6bb21f65900945db804bb9b2cda39de61cc1
bigseoulDeskTop <bigseoul@gmail.com>
2023-03-04 21:41:48 +0900

    del step02

--- Chapter02/step02/discount_condition_.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class AbsDiscountCondition(metaclass=ABCMeta):
-    """파이썬은 인터페이스가 없으므로 추상클래스로 구현"""
-
-    @abstractmethod
-    def is_satisfied_by(self, screening):
-        pass
